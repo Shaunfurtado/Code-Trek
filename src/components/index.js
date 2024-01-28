64028a1ad2b6bf2431293de0935a40db494a5f03
Shaunfurtado <shaunf1801@gmail.com>
2024-01-28 23:09:06 +0530

    Components

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar.jsx';
+import Footer from './Footer.jsx';
+import Form from './Form.jsx';
+import table from './Table.jsx';
+
+
+export {
+    Navbar,
+    Footer,
+    Form,
+    table
+};
