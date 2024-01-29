f1eb4f398974f35a17c55c7c020ba809381d30ad
Shaunfurtado <shaunf1801@gmail.com>
2024-01-29 22:11:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import low from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 import shortid from 'shortid';
 import cors from 'cors';
-import process from 'process';
 import bodyParser from 'body-parser';
+import process from 'process';
 
 const adapter = new FileSync('db.json');
 const db = low(adapter);
@@ -16,42 +16,30 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Fetch all problems
 app.get('/problems', (req, res) => {
-  const problems = db.get('problems').map(problem => ({
-    id: problem.id,
-    problemNumber: problem.problemNumber,
-    problemName: problem.problemName,
-    solvedDate: problem.solvedDate,
-    platformName: problem.platformName,
-    problemLink: problem.problemLink,
-    problemStatement: problem.problemStatement,
-    solution: problem.solution
-  })).value();
+  const problems = db.get('problems').value();
   res.json(problems);
 });
 
+// Create a new problem
 app.post('/problems', (req, res) => {
   const problem = req.body;
   problem.id = shortid.generate();
-  problem.problemNumber = req.body.problemNumber;
-  problem.problemName = req.body.problemName;
-  problem.solvedDate = req.body.solvedDate;
-  problem.platformName = req.body.platformName;
-  problem.problemLink = req.body.problemLink;
-  problem.problemStatement = req.body.problemStatement;
-  problem.solution = req.body.solution;
   db.get('problems').push(problem).write();
   res.json(problem);
 });
 
-app.put('/problems/:problemNumber', (req, res) => {
-  const updatedProblem = db.get('problems').find({ problemNumber: req.params.problemNumber }).assign(req.body).write();
+// Update a problem
+app.put('/problems/:id', (req, res) => {
+  const updatedProblem = db.get('problems').find({ id: req.params.id }).assign(req.body).write();
   res.json(updatedProblem);
 });
 
-app.delete('/problems/:problemNumber', (req, res) => {
-  db.get('problems').remove({ problemNumber: req.params.problemNumber }).write();
-  res.json({ problemNumber: req.params.problemNumber });
+// Delete a problem
+app.delete('/problems/:id', (req, res) => {
+  db.get('problems').remove({ id: req.params.id }).write();
+  res.json({ id: req.params.id });
 });
 
 const PORT = process.env.PORT || 3000;
